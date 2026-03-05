b7b8f29d54c02d82391124a932dc31a04c95b1c4
hernic <nicolas.hernandez@leph-networks.com>
2026-03-05 18:34:09 +0100

    Remove unused code

--- shared/src/main/java/com/aleph/graymatter/jtyposquatting/InvalidDomainException.java
+++ shared/src/main/java/com/aleph/graymatter/jtyposquatting/InvalidDomainException.java
@@ -4,8 +4,4 @@ public class InvalidDomainException extends Exception {
     public InvalidDomainException(String message) {
         super(message);
     }
-
-    public InvalidDomainException(String message, Throwable cause) {
-        super(message, cause);
-    }
 }
--- shared/src/main/java/com/aleph/graymatter/jtyposquatting/dto/GenerationRequestDTO.java
+++ shared/src/main/java/com/aleph/graymatter/jtyposquatting/dto/GenerationRequestDTO.java
@@ -1,5 +1,6 @@
 package com.aleph.graymatter.jtyposquatting.dto;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
 
@@ -9,69 +10,18 @@ import java.util.List;
  */
 public class GenerationRequestDTO implements Serializable {
     
-    private static final long serialVersionUID = 1L;
+    @Serial
+    private static final long serialVersionUID = 6117886166997374644L;
     
     private String baseDomain;
-    private List<String> algorithms;
     private boolean includeSubdomains;
     private boolean includeTLDVariations;
     private int maxResults;
-    
-    public GenerationRequestDTO() {
-    }
-    
-    public GenerationRequestDTO(String baseDomain) {
-        this.baseDomain = baseDomain;
-        this.includeSubdomains = true;
-        this.includeTLDVariations = true;
-        this.maxResults = 100;
-    }
-    
-    public String getBaseDomain() {
-        return baseDomain;
-    }
-    
-    public void setBaseDomain(String baseDomain) {
-        this.baseDomain = baseDomain;
-    }
-    
-    public List<String> getAlgorithms() {
-        return algorithms;
-    }
-    
-    public void setAlgorithms(List<String> algorithms) {
-        this.algorithms = algorithms;
-    }
-    
-    public boolean isIncludeSubdomains() {
-        return includeSubdomains;
-    }
-    
-    public void setIncludeSubdomains(boolean includeSubdomains) {
-        this.includeSubdomains = includeSubdomains;
-    }
-    
-    public boolean isIncludeTLDVariations() {
-        return includeTLDVariations;
-    }
-    
-    public void setIncludeTLDVariations(boolean includeTLDVariations) {
-        this.includeTLDVariations = includeTLDVariations;
-    }
-    
-    public int getMaxResults() {
-        return maxResults;
-    }
-    
-    public void setMaxResults(int maxResults) {
-        this.maxResults = maxResults;
-    }
-    
+
     @Override
     public String toString() {
         return "GenerationRequestDTO{" +
                 "baseDomain='" + baseDomain + '\'' +
-                ", algorithms=" + algorithms +
                 ", includeSubdomains=" + includeSubdomains +
                 ", includeTLDVariations=" + includeTLDVariations +
                 ", maxResults=" + maxResults +
